48a6de424e21adce404d43f13f8ee489ed4ad2a3
emran hossain <shuvoorba28@gmail.com>
2024-01-07 07:20:33 -0500

    basic navbar created

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../../Shared/Navbar/Navbar";
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <h3>This is home page</h3>
+        </div>
+    );
+};
+
+export default Home;
